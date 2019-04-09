6c162c6e07d62f7fd284ea893efd29b16fd52dec
Mads Mogensen <mads256h@gmail.com>
2019-04-09 13:18:35 +0200

    added shit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "avr-core/Pins.hpp"
 #include "avr-core/Serial.hpp"
 #include "avr-core/Util.hpp"
+#include "avr-core/Servo.hpp"
 
 #include <util/delay.h>
 #include <avr/power.h>
+
 
 int main()
 {
@@ -12,11 +14,36 @@ int main()
 
   Serial::Begin(115200);
 
-  Serial::TransmitInt<int8_t>(-128);
+  Pins::PinMode(9, true);
+  Pins::PinMode(10, true);
 
-  while (true)
-  {
-  }
+  Servo::Begin();
+
+  const uint8_t c = 1;
+
+  uint16_t i = Servo::OCR1A_MAX;
+  bool down = true;
+	
+  while(true)
+	{
+    if (i <= Servo::OCR1A_MIN) down = false;
+    if (i >= Servo::OCR1A_MAX) down = true;
+
+
+		if (down)
+    {
+      i -= c;
+    }
+
+    else
+    {
+      i += c;
+    }
+
+    Servo::SetPosition(i);
+
+    _delay_ms(1);
+	}
 
   return 0;
 }
